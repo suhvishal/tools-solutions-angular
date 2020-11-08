6b096ce34c40a12616cc74255eb099d937909f32
vishal <23210008@mkcl.org>
2020-11-08 19:05:31 +0530

    -

---
+++ demos/typescriptdemos/demo1.js
@@ -0,0 +1,8 @@
+var x = 10;
+if (x >= 10) {
+    var y = 30; //global
+    console.log('hello');
+    console.log('inside block, value of y = ' + y);
+}
+console.log('outside block, value of y = ' + y);
+
